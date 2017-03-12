e79e6824744b5a542216941c4af38545867b0692
panachai <panachai.ny@gmail.com>
2017-03-12 18:46:35 +0700

    Search complete

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,24 +1,138 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 
 public class SearchEngine extends javax.swing.JFrame {
 
-    DBHelper mydb;
+    private String database;
+    private String driver;
+    private String url;
+    private Connection conn;
+    private Statement stm;
+    private ResultSet rs;
+    private String sql;
+    Object cbCategory;
+
+    private DefaultTableModel model;
 
     public SearchEngine() {
-        mydb = new DBHelper();
-        /*
+        initComponents();
+        
+        bt_update.setEnabled(false);
+        bt_delete.setEnabled(false);
+        
+        bt_add.setEnabled(false);
+        bt_cancel.setEnabled(false);
+        
+        
+        tf_name.setEnabled(false);
+        tf_lastname.setEnabled(false);
+        tf_address.setEnabled(false);
+        
+        
+
+        model = (DefaultTableModel) tb_result.getModel();
+
+        database = "SearchEngineDb";
+        driver = "org.apache.derby.jdbc.ClientDriver";
+        url = "jdbc:derby:" + database + ";create=true";
+
         try {
-            mydb.selectAllProfile();
+            //step 1 load driver
+            Class.forName(driver).newInstance();
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("cannot load driver");
+            System.exit(-1);
+        } catch (InstantiationException ex) {
+        } catch (IllegalAccessException ix) {
+        }
+
+        try {
+            //step 2 connect to database
+            conn = DriverManager.getConnection(url, "", ""); //user password
+        } catch (SQLException ex) {
+            System.out.println("cannot load database");
+            System.exit(-1);
+        }
+
+        try {
+            //step 3
+            stm = conn.createStatement();
+        } catch (SQLException ex) {
+            System.out.println("cannot create statement");
+            System.exit(-1);
+        }
+
+        try {
+            selectAllProfile();
+
         } catch (SQLException ex) {
             System.out.println("Can't show All Profile");
             System.exit(-1);
         }
-        */
-
-        initComponents();
     }
 
+    public void insertProfile(String name, String lastname, String address) throws SQLException {
+        sql = "INSERT INTO profile VALUES('" + name + "','" + lastname + "','" + address + "')";
+        stm.executeUpdate(sql);
+
+    }
+
+    public void selectAllProfile() throws SQLException {
+        model.setRowCount(0);
+        
+        sql = "SELECT * FROM profile";
+        rs = stm.executeQuery(sql);
+        try {
+            while (rs.next()) {
+                model.addRow(new Object[]{rs.getString(1), rs.getString(2), rs.getString(3)});
+            }
+        } catch (SQLException ex) {
+            System.out.println("Can't show All Profile");
+            System.exit(-1);
+        }
+    }
+
+    public void searchProfile(String table, String msg) throws SQLException {
+        model.setRowCount(0);
+        
+        sql = "SELECT * FROM profile where " + table + " like '%" + msg + "%'";
+        rs = stm.executeQuery(sql);
+
+        while (rs.next()) {
+            model.addRow(new Object[]{rs.getString(1), rs.getString(2), rs.getString(3)});
+        }
+    }
+
+    public void deleteProfile(String field, String msg) throws SQLException {
+        sql = "DELETE FROM profile WHERE " + field + " = '" + msg + "'";
+        stm.executeUpdate(sql);
+    }
+
+    public void updateProfile() throws SQLException {
+        sql = "UPDATE profile SET name = 'Somkid' WHERE name = 'Somchai'";
+        stm.executeUpdate(sql);
+    }
+
+    /*
+    public void createTable() {
+        try {
+            //step 4 create table
+            sql = "CREATE TABLE profile(name VARCHAR(15),lastname VARCHAR(15),address VARCHAR(50))";
+            stm.execute(sql);
+
+        } catch (SQLException ex) {
+            System.out.println("cannot create table");
+            System.exit(-1);
+        }
+    }
+     */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -30,11 +144,11 @@ public class SearchEngine extends javax.swing.JFrame {
         tb_result = new javax.swing.JTable();
         cb_category = new javax.swing.JComboBox<>();
         bt_insert = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        tf_search1 = new javax.swing.JTextField();
-        tf_search2 = new javax.swing.JTextField();
-        tf_search3 = new javax.swing.JTextField();
+        bt_update = new javax.swing.JButton();
+        bt_delete = new javax.swing.JButton();
+        tf_name = new javax.swing.JTextField();
+        tf_lastname = new javax.swing.JTextField();
+        tf_address = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -53,24 +167,34 @@ public class SearchEngine extends javax.swing.JFrame {
 
         tb_result.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {},
-                {},
-                {},
-                {}
+
             },
             new String [] {
-
+                "NAME", "LASTNAME", "ADDRESS"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(tb_result);
 
         cb_category.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Name", "Lastname", "Address" }));
 
         bt_insert.setText("INSERT");
 
-        jButton3.setText("UPDATE");
+        bt_update.setText("UPDATE");
+        bt_update.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt_updateActionPerformed(evt);
+            }
+        });
 
-        jButton4.setText("DELETE");
+        bt_delete.setText("DELETE");
 
         jLabel1.setText("Name");
 
@@ -96,9 +220,9 @@ public class SearchEngine extends javax.swing.JFrame {
                             .addComponent(jLabel1))
                         .addGap(12, 12, 12)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(tf_search1, javax.swing.GroupLayout.DEFAULT_SIZE, 252, Short.MAX_VALUE)
-                            .addComponent(tf_search2)
-                            .addComponent(tf_search3))
+                            .addComponent(tf_name, javax.swing.GroupLayout.DEFAULT_SIZE, 252, Short.MAX_VALUE)
+                            .addComponent(tf_lastname)
+                            .addComponent(tf_address))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(bt_cancel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -115,9 +239,9 @@ public class SearchEngine extends javax.swing.JFrame {
                                 .addComponent(bt_search, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(bt_update, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(bt_insert, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(bt_delete, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
@@ -134,22 +258,22 @@ public class SearchEngine extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(bt_insert)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton3)
+                        .addComponent(bt_update)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton4)))
+                        .addComponent(bt_delete)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(tf_search1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(tf_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel1))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(tf_search2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(tf_lastname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel2))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(tf_search3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(tf_address, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(18, 18, 18)
@@ -164,16 +288,28 @@ public class SearchEngine extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void bt_searchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_searchActionPerformed
-        System.out.println("test : " + tf_search.getText());
-        try{
-            mydb.insertProfile("คนไทย","หัวใจ","โค๊ดดิ่ง");
-        mydb.selectAllProfile();
-        }catch(SQLException se){
-            System.out.println("error search : "+se);
+        String msg = tf_search.getText();   //get search bar
+        cbCategory = cb_category.getSelectedItem();
+        if (cbCategory != null) {
+            String selectedItemStr = cbCategory.toString();
+            
+            System.out.println("test combobox : "+selectedItemStr);
+
+            try {
+                searchProfile(selectedItemStr, msg);
+
+            } catch (SQLException se) {
+                System.out.println("Error search : " + se);
+            }
+
         }
-        
-        
+
+
     }//GEN-LAST:event_bt_searchActionPerformed
+
+    private void bt_updateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_updateActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bt_updateActionPerformed
 
     public static void main(String args[]) {
 
@@ -207,20 +343,20 @@ public class SearchEngine extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bt_add;
     private javax.swing.JButton bt_cancel;
+    private javax.swing.JButton bt_delete;
     private javax.swing.JButton bt_insert;
     private javax.swing.JButton bt_search;
+    private javax.swing.JButton bt_update;
     private javax.swing.JComboBox<String> cb_category;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTable tb_result;
+    private javax.swing.JTextField tf_address;
+    private javax.swing.JTextField tf_lastname;
+    private javax.swing.JTextField tf_name;
     private javax.swing.JTextField tf_search;
-    private javax.swing.JTextField tf_search1;
-    private javax.swing.JTextField tf_search2;
-    private javax.swing.JTextField tf_search3;
     // End of variables declaration//GEN-END:variables
 }
